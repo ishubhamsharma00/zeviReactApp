4ec8f1f6ce374306ceccd69afe19322c305f67eb
Shubham Sharma <ishubhamsharma@Shubhams-MacBook-Pro.local>
2023-07-29 00:34:10 +0530

    fakerjs dynamic  data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import SearchBar from "./components/SearchBar";
 
 function App() {
@@ -5,15 +6,16 @@ function App() {
     <>
       <div className="h-screen bg-[#D7E0FF42] w-full">
         <img src='https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhbmdpbmclMjBjbG90aGVzfGVufDB8fDB8fHww&w=1000&q=80'
-        alt='backgroundImg'
-        className="absolute h-screen w-full -z-20"/>
+          alt='backgroundImg'
+          className="absolute h-screen w-full -z-20"
+        />
         <div className="w-full flex items-center justify-end p-6">
           <img src='https://uploads-ssl.webflow.com/622778f0460ef2a7b46117c1/632c38d0a82442dc8dfe0f53_zevi-logo-_2_.webp'
             alt='logo'
             className="w-20 h-10 -z-10" 
           />
         </div>
-        <div className='flex items-center justify-center w-full mt-4'>
+        <div className=''>
           <SearchBar/>
         </div>
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
-import {RiSearchLine} from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiSearchLine } from 'react-icons/ri';
+import { faker } from '@faker-js/faker';
+import { Link } from 'react-router-dom';
 
 const SearchBar = () => {
+  const [input, setInput] = useState('');
+  const [open, setOpen] = useState(false);
+  const clothingItems = Array.from({ length: 5 }, (_, index) => ({
+    name: faker.commerce.productName(),
+    photo: faker.image.fashion(640,480,true),
+  }));
+  const trendingItems= Array.from({length:5},(index)=>(faker.commerce.productName()));
+  const handleEnter=(e)=>{
+    if(e.key==='Enter')
+    {
+      console.log(input);
+    }
+  }
 
   return (
-    <div className='flex items-center justify-between h-14 w-3/5 bg-[#FFFFFF] px-6 text-xl rounded-xl outline-none shadow-xl py-2'>
-        <input 
-        type='search'
-        placeholder='Search'
-        className='searchbar bg-white w-4/5 outline-none placeholder:text-[#00000066] placeholder:font-inter placeholder:font-normal'
+    <div className='h-full flex flex-col items-center justify-center w-full mt-4'>
+      <div className='flex items-center justify-between h-14 w-3/5 bg-[#FFFFFF] px-6 text-xl rounded-xl outline-none shadow-xl py-2'
+        onClick={() => setOpen(true)}>
+        <input
+          type='search'
+          placeholder='Search'
+          className='bg-white w-full outline-none placeholder:text-[#00000066] placeholder:font-inter placeholder:font-normal'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={handleEnter}
         />
-        <RiSearchLine className='h-8 w-8 text-gray-400 '/>
+        <RiSearchLine className='h-8 w-8 text-gray-400 ' />
+      </div>
+      {open && (
+        <div className='flex flex-col bg-white rounded-[4px] w-2/3 h-full mt-4 p-4 font-inter' >
+          <div className='flex p-1 flex-col'>
+            <p className='font-[400px] text-[20px]'>Latest Trends</p>
+            <div className='grid grid-cols-5 gap-4 mt-2'>
+              {clothingItems.map((item,index) => (
+                <div key={index} className='flex flex-col'>
+                  <img src={item.photo}
+                    alt='trandingImages'
+                    className='w-[155px] h-[200px]' />
+                  <p className='text-xs mt-2 font-extralight'>{item.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='my-4 p-1'>
+            <p className='font-[400px] text-[20px]'>Popular suggestions</p>
+            <div className='mt-2'>
+              {trendingItems.map((items)=>(
+                <p className='font-light text-xs mt-1'>
+                  {items}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
